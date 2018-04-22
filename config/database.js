81ede6cf55bf51399e73bec5122ff0a62769dc1b
j-vandervaart <jvand223@gmail.com>
2018-04-22 14:59:16 -0400

    test

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information
-  database: 'mongodb://jessy:jessy@ds253889.mlab.com:53889/portfoliodb',   //prod
+  database: 'mongodb://jessy:jessy@ds253889.mlab.com:53889/portfolio_db',   //prod
   //database: 'mongodb://localhost:27017/meanauth',    //dev
 }
